e30ed8513d78d613a5e70d0b5405ff4056096060
Lucas Frazão <lucas_devfrazao@outlook.com>
2023-06-20 16:01:17 -0300

    CameraSwitch script updated

--- Assets/S05_05/Graphs/CameraSwitch.cs
+++ Assets/S05_05/Graphs/CameraSwitch.cs
@@ -4,34 +4,42 @@ using UnityEngine;
 
 public class CameraSwitch : MonoBehaviour
 {
-    [SerializeField] private Transform tank; // Referência ao transform do objeto do tanque
-    [SerializeField] private Camera[] cameras; // Array contendo todas as câmeras
+    [SerializeField] private Transform tank;
+    [SerializeField] private Camera[] cameras;
+    private Camera activeCamera;
+
+    void Start()
+    {
+        foreach (Camera camera in cameras)
+        {
+            camera.gameObject.SetActive(false);
+        }
+    }
 
     void Update()
     {
-        // Verifica se o tanque está visível em alguma câmera
-        bool isTankVisible = false;
+        activeCamera = FindActiveCamera();
+
         foreach (Camera camera in cameras)
         {
-            // Converte a posição do tanque para as coordenadas da viewport da câmera
+            camera.gameObject.SetActive(camera == activeCamera);
+            camera.enabled = camera == activeCamera;
+        }
+    }
+
+    Camera FindActiveCamera()
+    {
+        foreach (Camera camera in cameras)
+        {
             Vector3 viewportPoint = camera.WorldToViewportPoint(tank.position);
 
             // Verifica se o tanque está dentro da área visível da câmera
             if (viewportPoint.x >= 0 && viewportPoint.x <= 1 && viewportPoint.y >= 0 && viewportPoint.y <= 1 && viewportPoint.z > 0)
             {
-                isTankVisible = true;
-                break;
+                return camera;
             }
         }
 
-        // Ativa a câmera correta com base na visibilidade do tanque
-        for (int i = 0; i < cameras.Length; i++)
-        {
-            // Ativa a câmera apenas se o tanque estiver visível e for a última câmera no array
-            cameras[i].gameObject.SetActive(isTankVisible && i == cameras.Length - 1);
-
-            // Ativa ou desativa o componente Camera da câmera
-            cameras[i].enabled = cameras[i].gameObject.activeSelf;
-        }
+        return null; // Retorna nulo se o tanque não estiver visível em nenhuma câmera
     }
 }
