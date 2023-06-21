4b9bc97c006eae136c37429dce961c9585f7b6bc
Lucas Frazão <lucas_devfrazao@outlook.com>
2023-06-21 13:55:57 -0300

    SFX added

--- Assets/Resources/Scripts/FollowWP.cs
+++ Assets/Resources/Scripts/FollowWP.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class FollowWP : MonoBehaviour
@@ -12,16 +11,45 @@ public class FollowWP : MonoBehaviour
 
     [SerializeField] private GameObject _wpManager;
     [SerializeField] private GameObject[] _wps;
+    private AudioSource _tankAudioSource;
     private GameObject _currentNode;
     private int _currentWP = 0;
     private Graph _graph;
 
-    // Start is called before the first frame update
+    private void Awake()
+    {
+        _tankAudioSource = GetComponent<AudioSource>();
+    }
+
     private void Start()
     {
         _wps = _wpManager.GetComponent<WPManager>().Waypoints;
         _graph = _wpManager.GetComponent<WPManager>().Graph;
         _currentNode = _wps[0];
+    }
+
+    private void LateUpdate()
+    {
+        if (_graph.PathList.Count == 0 || _currentWP == _graph.PathList.Count)
+            return;
+
+        if (Vector3.Distance(_graph.PathList[_currentWP].getId().transform.position, transform.position) < _accuracy)
+        {
+            _currentNode = _graph.PathList[_currentWP].getId();
+            _currentWP++;
+        }
+
+        if (_currentWP < _graph.PathList.Count)
+        {
+            _goal = _graph.PathList[_currentWP].getId().transform;
+            Vector3 lookAtGoal = new Vector3(_goal.position.x, transform.position.y, _goal.position.z);
+            Vector3 direction = lookAtGoal - transform.position;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * _rotSpeed);
+
+            transform.Translate(0, 0, _speed * Time.deltaTime);
+        }
+
+        StopSFX();
     }
 
     public void GoToDestination(int destinationIndex)
@@ -48,27 +76,24 @@ public class FollowWP : MonoBehaviour
                 Debug.LogError("Destination index not recognized.");
                 break;
         }
+
+        PlaySFX();
     }
 
-    private void LateUpdate()
+    private void PlaySFX()
     {
-        if (_graph.PathList.Count == 0 || _currentWP == _graph.PathList.Count)
-            return;
-
-        if (Vector3.Distance(_graph.PathList[_currentWP].getId().transform.position, transform.position) < _accuracy)
+        if (_tankAudioSource != null && _tankAudioSource.clip != null && !_tankAudioSource.isPlaying)
         {
-            _currentNode = _graph.PathList[_currentWP].getId();
-            _currentWP++;
-        }
-
-        if (_currentWP < _graph.PathList.Count)
-        {
-            _goal = _graph.PathList[_currentWP].getId().transform;
-            Vector3 lookAtGoal = new Vector3(_goal.position.x, transform.position.y, _goal.position.z);
-            Vector3 direction = lookAtGoal - transform.position;
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * _rotSpeed);
-
-            transform.Translate(0, 0, _speed * Time.deltaTime);
+            _tankAudioSource.Play();
         }
     }
+
+    private void StopSFX()
+    {
+        if (_tankAudioSource != null && _tankAudioSource.clip != null && _tankAudioSource.isPlaying && _currentWP == _graph.PathList.Count)
+        {
+            _tankAudioSource.Stop();
+        }
+    }
+
 }
